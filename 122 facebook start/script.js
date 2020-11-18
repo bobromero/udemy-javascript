866a1c0503c9d485a6f7966524fff54976f2120f
rromero123102 <rromero21332@columbushs.com>
2020-11-18 10:16:21 -0500

    122

---
+++ 122 facebook start/script.js
@@ -0,0 +1,29 @@
+var user = {
+    username: "ROb",
+    password: "1234455"
+}
+var database = [
+    {
+        username: "ROb",
+        password: "1234455"
+    
+    }
+
+]
+
+var newsfeed = [
+    {
+        username: "ROb",
+        timeline: "I AM ROB"
+    },
+    
+    {
+        username: "JOe",
+        timeline: "I AM JOE"
+    },
+    {
+        username: "BOb",
+        timeline: "I AM BOB"
+    }
+
+]
